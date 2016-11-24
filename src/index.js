b89a6b3dd29cf6122ddad8b5189f5af701a34596
Vazha Omanashvili <rainum@users.noreply.github.com>
2016-11-24 19:20:56 +0200

    expose query helpers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {runLogic} from './logic/index';
 import {buildPathSelector} from './utils/strings';
 import * as VariableHelpers from './variables/index';
 import * as JSONHelpers from './utils/json';
+import * as QueryHelpers from './utils/query';
 
 export default {
   generateAuthPatch,
@@ -10,4 +11,5 @@ export default {
   buildPathSelector,
   ...VariableHelpers,
   ...JSONHelpers,
+  ...QueryHelpers,
 };
